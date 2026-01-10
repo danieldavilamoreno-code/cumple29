f485e9dbd33c82eb6cb02ed795e55bdbf39e78fd
danieldavilamoreno-code <daniel.davila.moreno@gmail.com>
2026-01-10 10:20:40 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Importar Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Tu configuración (REEMPLAZA con la tuya)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+export const db = getFirestore(app);
