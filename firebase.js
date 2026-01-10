4aa94df16d75989765e422479d6157efbdcfde0a
danieldavilamoreno-code <daniel.davila.moreno@gmail.com>
2026-01-10 10:22:48 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Tu configuración (REEMPLAZA con la tuya)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDVf2aqLH5kte0RYwxki43eJVwKPApSEs0",
+  authDomain: "cumple29-2871f.firebaseapp.com",
+  projectId: "cumple29-2871f",
+  storageBucket: "cumple29-2871f.firebasestorage.app",
+  messagingSenderId: "109039407228",
+  appId: "1:109039407228:web:13ac3e5b54c129e097b9f9"
 };
 
 // Inicializar Firebase
